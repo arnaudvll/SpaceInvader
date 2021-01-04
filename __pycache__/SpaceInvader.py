112aa53eb161dfd939c316fa21d09590ce6baefc
alexandre858 <alex.schurck@gmail.com>
2021-01-04 16:12:54 +0100

    mise a jour

--- __pycache__/SpaceInvader.py
+++ __pycache__/SpaceInvader.py
@@ -1,33 +1,41 @@
 from tkinter import *
 
 
+
 fenetre = Tk()
+
 fenetre.geometry("1200x900")
+
 Label(fenetre, text = 'score').pack(padx = 20,pady = 0)
+
 Button(fenetre, text = 'Nouvelle partie').pack(side = "left" )
 Button(fenetre, text = 'Quitter')
 
-bg=Canvas (fenetre, width="1200",height="900" ,bg='blue')
+image_vaisseau = PhotoImage(file ="img_vais.ppm")
+bg=Canvas (fenetre, width="1920",height="1080" ,bg='black')
+
+
+vaisseau=bg.create_image(250,800, anchor = NW, image=image_vaisseau )
 bg.pack()
-vesseau=bg.create_rectangle(580, 800,620,840,outline='red',fill='white' )
 def deplacement(event):
-    global vesseau
+    global vaisseau
     global bg
     key=event.keysym
     print(key)
-    xmin=bg.coords(vesseau)[0]
-    xmax=bg.coords(vesseau)[2]
-    if xmin>0 :
-        if key=="Left":
-            bg.move(vesseau, -20  , 0 )
-        if key=="Right":
-            bg.move(vesseau, 20  , 0 )
-    if xmin<0 :
-        if key=="Left":
-            bg.move(vesseau, -20  , 0 )
-        if key=="Right":
-            bg.move(vesseau, 20  , 0 )
+    xmin=bg.coords(vaisseau)[0]
 
+    print(xmin)
+    if xmin>20 and xmin <1030:
+        if key=="Left":
+            bg.move(vaisseau, -35  , 0 )
+        if key=="Right":
+            bg.move(vaisseau, 35  , 0 )
+    if xmin<=20 :
+        if key=="Right":
+            bg.move(vaisseau, 35  , 0 )
+    if xmin >=1030:
+        if key=="Left":
+            bg.move(vaisseau, -35  , 0 )
 
 bg.focus_set()
 bg.bind_all('<Key>',deplacement)
@@ -35,3 +43,4 @@ bg.bind_all('<Key>',deplacement)
 
     
 fenetre.mainloop()
+
